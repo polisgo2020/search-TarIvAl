6473f44c178647ed425eb8b27f1ba560837a0776
Ivan Tarasov <tarasovia1998@gmail.com>
2020-03-18 15:31:42 +0300

    Add stopwords, stemmer and words positions in index

--- index/index.go
+++ index/index.go
@@ -1,45 +1,93 @@
 package index
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 	"unicode"
+
+	"github.com/kljensen/snowball/english"
 )
-// IndexReverse is type for storage reverse index in program
-type IndexReverse map[string][]string
+
+type wordIndex struct {
+	File      string
+	Positions []int
+}
+
+// ReverseIndex is type for storage reverse index in program
+type ReverseIndex map[string][]wordIndex
+
+// CreateStopWordsMap - create map stopWords
+func CreateStopWordsMap(path string) map[string]bool {
+	fileStopWords, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stopWords := strings.Fields(string(fileStopWords))
+
+	mapStopWords := map[string]bool{}
+	for _, stopWord := range stopWords {
+		mapStopWords[strings.ToLower(stopWord)] = true
+	}
+	return mapStopWords
+}
 
 // IndexingFolder create a file with revrse index
-func IndexingFolder(path string) {
+func IndexingFolder(path, pathToStopWords string) ReverseIndex {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	index := make(IndexReverse)
 
-	for _, f := range files {
-		file, err := ioutil.ReadFile(path + "\\" + f.Name())
-		if err != nil {
-			log.Fatal(err)
-		}
+	mapStopWords := CreateStopWordsMap(pathToStopWords)
+	index := make(ReverseIndex)
 
-		words := strings.Fields(string(file))
-
-		for _, word := range words {
-			word = strings.TrimFunc(string(word), func(r rune) bool {
-				return !unicode.IsLetter(r) && !unicode.IsNumber(r)
-			})
-			word = strings.ToLower(word)
-			index[word] = append(index[word], f.Name())
-		}
+	for _, file := range files {
+		addFileInIndex(file, path, mapStopWords, index)
 	}
 
-	output, err := json.Marshal(index)
+	return index
+}
+
+func hasFileInIndex(sliceIndex []wordIndex, fileName string) (int, bool) {
+	for i, indexWord := range sliceIndex {
+		if indexWord.File == fileName {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+func addFileInIndex(file os.FileInfo, path string, mapStopWords map[string]bool, index ReverseIndex) {
+	fileText, err := ioutil.ReadFile(path + string(os.PathSeparator) + file.Name())
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := ioutil.WriteFile("index.json", output, 0); err != nil {
-		log.Fatal(err)
+
+	words := strings.Fields(string(fileText))
+
+	for i, word := range words {
+		word = strings.TrimFunc(word, func(r rune) bool {
+			return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+		})
+		word = strings.ToLower(word)
+		word = english.Stem(word, false)
+		if _, ok := mapStopWords[word]; ok || word == "" {
+			continue
+		}
+
+		if sliceIndex, ok := index[word]; ok {
+			if j, ok := hasFileInIndex(sliceIndex, file.Name()); ok {
+				index[word][j].Positions = append(index[word][j].Positions, i)
+				continue
+			}
+		}
+
+		item := wordIndex{
+			File:      file.Name(),
+			Positions: []int{i},
+		}
+		index[word] = append(index[word], item)
 	}
 }
